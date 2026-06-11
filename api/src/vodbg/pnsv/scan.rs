1f25ef712f1bd9d1795fbc16a3ca3a8d189dfab4
headless-axolotl <martin.kostadinov05@gmail.com>
2026-06-11 10:00:50 +0300

    Fixed a few bugs in the LcsSimd scanning implementation and wrote tests for it.

--- api/src/vodbg/pnsv/scan.rs
+++ api/src/vodbg/pnsv/scan.rs
@@ -18,10 +18,11 @@ impl LcsSimd {
         T: Into<u8>,
         I: Iterator<Item = T>
     {
-        let mut words = vec![];
+        #[allow(clippy::manual_div_ceil)]
+        let mut words = Vec::with_capacity((n + Self::LANES - 1) / Self::LANES);
         let mut array = Self::ZERO;
         for (i, item) in input.enumerate() {
-            if (i + 1) % Self::LANES == 0 {
+            if i != 0 && i % Self::LANES == 0 {
                 words.push(Word::new(array));
                 array = Self::ZERO;
             }
@@ -33,6 +34,10 @@ impl LcsSimd {
     }
 
     pub fn scan_left(&self, index: usize, target_length: u8) -> usize {
+        if index >= self.n {
+            return self.n;
+        }
+
         let word_index = index / Self::LANES;
         let index_in_word = index % Self::LANES;
 
@@ -59,8 +64,12 @@ impl LcsSimd {
     }
 
     pub fn scan_right(&self, index: usize, target_length: u8) -> usize {
+        if index >= self.n {
+            return self.n;
+        }
+
         let word_index = index / Self::LANES;
-        let index_in_word = (index % Self::LANES) + 1;
+        let index_in_word = index % Self::LANES;
 
         // Similarly to the scan_left procedure, first scan values in the word the index is located
         // in individually.
@@ -102,6 +111,77 @@ impl ContractLeft for LcsSimd {
         let new_start = self.scan_left(I.start, target_len as u8);
         let new_end = self.scan_right(I.end, target_len as u8);
         new_start..new_end
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lcs_simd_words_are_correct() {
+        let items: &[u8] = &[
+             0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
+            10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+        ];
+
+        let lcs_simd = LcsSimd::from_iterator(items.iter().cloned(), items.len());
+        for i in 0..items.len() {
+            let item_in_word = lcs_simd.words[i / LcsSimd::LANES].as_array()[i % LcsSimd::LANES];
+            assert_eq!(items[i], item_in_word);
+        }
+    }
+
+    #[test]
+    fn lcs_simd_scan_left() {
+        let items: &[u8] = &[
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 5, 5, 5, 5, 5, 5, // 25: 1
+
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 5, 5, 5, 5, 5, 5, // 57: 2
+
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 5, 5, 5, 5, 5, 5, // 89: 3
+
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 5, 5, 5, 5, 5, 5, // 121: 4
+        ];
+
+        let lcs_simd = LcsSimd::from_iterator(items.iter().cloned(), items.len());
+        assert_eq!(lcs_simd.scan_left(125, 5), 121, "Result in the same SIMD word failed.");
+        assert_eq!(lcs_simd.scan_left(100, 5), 89, "Result in the previous SIMD word failed.");
+        assert_eq!(lcs_simd.scan_left(100, 4), 89, "Smaller than 4 failed.");
+        assert_eq!(lcs_simd.scan_left(100, 3), 57, "Smaller than 3 failed.");
+        assert_eq!(lcs_simd.scan_left(100, 2), 25, "Smaller than 2 failed.");
+        assert_eq!(lcs_simd.scan_left(100, 1), 0, "Scan to beginning failed.");
+    }
+
+    #[test]
+    fn lcs_simd_scan_right() {
+        let items: &[u8] = &[
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 5, 5, 5, 5, 5, 5, // 25: 4
+
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 3, 5, 5, 5, 5, 5, 5, // 57: 3
+
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 2, 5, 5, 5, 5, 5, 5, // 89: 2
+
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 1, 5, 5, 5, 5, 5, 5, // 121: 1
+        ];
+
+        let lcs_simd = LcsSimd::from_iterator(items.iter().cloned(), items.len());
+        assert_eq!(lcs_simd.scan_right(20, 5), 25, "Result in the same SIMD word failed.");
+        assert_eq!(lcs_simd.scan_right(30, 5), 57, "Result in the next SIMD word failed.");
+        assert_eq!(lcs_simd.scan_right(30, 4), 57, "Smaller than 4 failed.");
+        assert_eq!(lcs_simd.scan_right(30, 3), 89, "Smaller than 3 failed.");
+        assert_eq!(lcs_simd.scan_right(30, 2), 121, "Smaller than 2 failed.");
+        assert_eq!(lcs_simd.scan_right(30, 1), items.len(), "Scan to the end failed.");
     }
 }
 
