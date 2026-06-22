61d2f98b8571c309597c0057916c4665dbcee456
headless-axolotl <martin.kostadinov05@gmail.com>
2026-06-22 08:46:19 +0300

    Added PnsvTuned structure.

--- api/src/vodbg/pnsv.rs
+++ api/src/vodbg/pnsv.rs
@@ -102,7 +102,7 @@ impl Pnsv for PnsvDynOwned {
 
 // Experimentally the scan is fastest if the average length of the ranges it searches is below
 // around 200 i.e. the target length. This value is equal to approx log_4(200).
-const TARGET_LENGTH_LOG_4_FLOOR: usize = 4;
+const TARGET_LENGTH_LOG_4_FLOOR: usize = 3;
 
 pub fn pnsv_simd_fallback_matrix(extend: &impl ExtendRight, lcs: &LcsArray, scan_bound: usize) -> PnsvDynOwned {
     let count = lcs.len();
@@ -246,6 +246,106 @@ pub fn pnsv_abs_simd(extend: &impl ExtendRight, lcs: &LcsArray) -> PnsvDynOwned 
 
     PnsvDynOwned {
         structures: vec![ranges_box, abs_box],
+    }
+}
+
+pub struct PnsvTuned {
+    pub ranges: Ranges,
+    pub matrix: PnsvMatrixSux,
+    pub lcs_simd: LcsSimd,
+    pub scan_bound: usize,
+    pub fallback_scan_overlap: usize,
+}
+
+impl PnsvTuned {
+    pub const DEFAULT_SCAN_BOUND: usize = 4;
+    pub const DEFAULT_FALLBACK_OVERLAP: usize = 2;
+
+    pub fn new_with_default_values(extend: &impl ExtendRight, lcs: &LcsArray) -> Self {
+        Self::new(extend, lcs, Self::DEFAULT_SCAN_BOUND, Self::DEFAULT_FALLBACK_OVERLAP)
+    }
+
+    pub fn new(extend: &impl ExtendRight, lcs: &LcsArray, scan_bound: usize, fallback_scan_overlap: usize) -> Self {
+        let count = lcs.len();
+
+        log::info!("[PnsvTuned::new] creating ranges...");
+        let mut ranges_upper_bound = 0;
+        let mut bits_in_current_level_of_ranges = usize::BITS as usize * 4;
+        while bits_in_current_level_of_ranges < count {
+            ranges_upper_bound += 1;
+            bits_in_current_level_of_ranges *= 4;
+        }
+        ranges_upper_bound = ranges_upper_bound.min(Ranges::MAX_K);
+        let ranges = Ranges::new(extend, count, ranges_upper_bound);
+
+        let iterator = (0..count).map(|index| lcs.access(index) as u8);
+
+        let log_4 = (usize::BITS - count.leading_zeros()).div_ceil(2) as usize;
+        let mut matrix_upper_bound = log_4 - TARGET_LENGTH_LOG_4_FLOOR; 
+        matrix_upper_bound = matrix_upper_bound.min(ranges_upper_bound + 1 + matrix::MAX_ROWS);
+
+        log::info!("[PnsvTuned::new] creating matrix...");
+        let matrix = PnsvMatrixSux::from_iterator(iterator.clone(), count, ranges_upper_bound + 1, matrix_upper_bound);
+        assert!(matrix.max_target() >= fallback_scan_overlap, "Make sure the fallback scan overlap has a reasonably small value (i.e. most 5).");
+
+        log::info!("[PnsvTuned::new] creating lcs simd...");
+        let lcs_simd = LcsSimd::from_iterator(iterator, count);
+
+        log::info!("[PnsvTuned::new] target length ranges: 1:{}:{}:..", ranges_upper_bound, matrix_upper_bound);
+
+        Self {
+            ranges,
+            matrix,
+            lcs_simd,
+            scan_bound,
+            fallback_scan_overlap,
+        }
+    }
+}
+
+impl Pnsv for PnsvTuned {
+    fn previous(&self, index: usize, target_length: usize) -> usize {
+        if target_length <= self.ranges.max_target() {
+            return self.ranges.previous(index, target_length);
+        }
+
+        if target_length <= self.matrix.max_target() - self.fallback_scan_overlap {
+            return self.ranges.previous(index, target_length);
+        }
+
+        if target_length <= self.matrix.max_target() {
+            let result = self.lcs_simd.scan_left_bounded(index, target_length as u8, self.scan_bound);
+            return match result {
+                Ok(index) => index,
+                Err(continue_search_index) => {
+                    self.matrix.previous(continue_search_index, target_length)
+                }
+            };
+        }
+
+        self.lcs_simd.scan_left(index, target_length as u8)
+    }
+
+    fn next(&self, index: usize, target_length: usize) -> usize {
+        if target_length <= self.ranges.max_target() {
+            return self.ranges.next(index, target_length);
+        }
+
+        if target_length <= self.matrix.max_target() - self.fallback_scan_overlap {
+            return self.ranges.next(index, target_length);
+        }
+
+        if target_length <= self.matrix.max_target() {
+            let result = self.lcs_simd.scan_right_bounded(index, target_length as u8, self.scan_bound);
+            return match result {
+                Ok(index) => index,
+                Err(continue_search_index) => {
+                    self.matrix.next(continue_search_index, target_length)
+                }
+            };
+        }
+
+        self.lcs_simd.scan_right(index, target_length as u8)
     }
 }
 
